3be9ee4c6941516457bc968cbae104b5cad362a0
Sofinam <omukowus@gmail.com>
2025-02-09 21:37:54 +0100

    Delete Desktop directory
    deleted desktop folder

--- Desktop/one button/application/static/css/index.js
+++
@@ -1,8 +0,0 @@
-function deleteNote(noteid) {
-    fetch('/delete-note', {
-        method: 'POST',
-        body: JSON.stringify({ noteid: noteid})
-    }).then((_res)=> {
-        window.location.href = "/";
-    })
-}
